af249e77a45240c5716ce99dc41b5f4e4a721902
almas615 <almasadlilwafi615@gmail.com>
2021-08-27 17:41:56 +0700

    menambahkan folder static untuk asset

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+app.get('/',(req, res)=>{
+    res.render('index');
+});
+
+app.get('/game',(req, res)=>{
+    res.render('gameSuit');
+});
+
+app.listen(port);
