dd92a90288599c81b1fc6df4ca21526b7343df6d
almas615 <almasadlilwafi615@gmail.com>
2021-08-27 23:36:45 +0700

    tambah login

--- index.js
+++ index.js
@@ -1,17 +1,49 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 const port = 3000;
+ let users = require('./db/user.json');
 
 
 app.set('view engine', 'ejs');
+app.use(express.json());
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: false }));
+
 
 app.get('/',(req, res)=>{
-    res.render('index');
+    data = {
+        judul : "home",
+        email : req.query.email
+    }
+    res.render('index',data);
 });
 
 app.get('/game',(req, res)=>{
-    res.render('gameSuit');
+    data = {
+        judul : "Game Suit"
+    }
+    res.render('gameSuit', data);
 });
+
+app.post('/login', (req, res) => {
+    let get = [];
+    get =users.find((item) => {
+      if (item.email == req.body.email && item.password == req.body.password) {
+          return item;
+      }
+    });
+    if(get == undefined){
+        res.redirect('/');
+    } else{
+        res.redirect('/?email='+get.email);
+    }
+    
+  });
+
+app.get('/login',(req, res)=>{
+    res.render('login');
+});
+// app.post('/')
 
 app.listen(port);
