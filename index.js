07f1c4e08fd06cbe64df26744e6a7a002f8f0957
almas615 <almasadlilwafi615@gmail.com>
2021-08-29 09:56:08 +0700

    tambah fungsi logout, login dan register

--- index.js
+++ index.js
@@ -1,24 +1,39 @@
 const express = require('express');
 const app = express();
+const fs = require('fs');
 const bodyParser = require('body-parser');
+const sessions = require('express-session');
+
 const port = 3000;
- let users = require('./db/user.json');
+ 
+let users = require('./db/user.json');
 
 
 app.set('view engine', 'ejs');
 app.use(express.json());
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(sessions({secret: 'ssshhhhh',saveUninitialized: true,resave: true}));
 
 
+
+
+
+// routing halaman home
 app.get('/',(req, res)=>{
     data = {
         judul : "home",
-        email : req.query.email
+        email: req.session.email
     }
-    res.render('index',data);
+    res.render('index',data,console.log(req.session.email));
 });
 
+// get all users
+app.get('/users',(req, res)=>{
+    res.status(200).json(users);
+})
+
+// routing halaman game
 app.get('/game',(req, res)=>{
     data = {
         judul : "Game Suit"
@@ -26,24 +41,70 @@ app.get('/game',(req, res)=>{
     res.render('gameSuit', data);
 });
 
+// login proses
 app.post('/login', (req, res) => {
+    const sess = req.session;
     let get = [];
     get =users.find((item) => {
-      if (item.email == req.body.email && item.password == req.body.password) {
-          return item;
-      }
+        if (item.email == req.body.email && item.password == req.body.password) {
+            return item;
+        }
     });
     if(get == undefined){
-        res.redirect('/');
+        res.redirect('/login');
     } else{
-        res.redirect('/?email='+get.email);
+        sess.email = req.body.email;
+        res.redirect(`/`);
     }
     
-  });
+});
 
+// login halaman
 app.get('/login',(req, res)=>{
     res.render('login');
 });
+
+
+// register prosess
+app.post('/register',(req, res)=>{
+    const user = {
+        nama : req.body.nama,
+        email : req.body.email,
+        password : req.body.password
+    }
+    let get =[];
+    get = users.find((item) => {
+        if (item.email == req.body.email) {
+            return item;
+        }
+    });
+
+    if (get == undefined) {
+        // const dataFileUser = fs.readFileSync('db/user.json', 'utf8'); // ambilsemua data pada json
+        // const dataUsers = JSON.parse(dataFileUser); //masukan data pada file json ke dalam objec users
+        
+        users.push(user); // tambahkan data user baru ke dalam users
+        
+        fs.writeFileSync('db/user.json', JSON.stringify(users)); // timpa data file json dengan data baru
+        
+        res.redirect('/');
+    }else{
+        res.redirect('/register');
+    }
+    
+    
+})
+
+// halamat register
+app.get('/register',(req, res)=>{
+    res.render('register');
+})
+
+// proses logout
+app.get('/logout',(req, res)=>{
+    req.session.destroy();
+    res.redirect('/');
+})
 // app.post('/')
 
 app.listen(port);
