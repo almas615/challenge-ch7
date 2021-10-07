0184fc5698ac018b1bee0171c2b41f36cf054f14
almas615 <amasadlilwafi615@gmail.com>
2021-10-07 13:43:15 +0700

    tambah rute create-room

--- router/gameRouter.js
+++ router/gameRouter.js
@@ -3,5 +3,7 @@ const game = require("../controllers/gameController")
 const restrict = require('../middlewares/restrict')
 
 router.get('/', game.getGamePage);
+router.get('/test/:idRoom', game.test);
+router.post('/create-room', restrict, game.createRoom);
 
 module.exports = router;
