a32facbd428c7b5e6d4754088c610732cd669b1f
almas615 <amasadlilwafi615@gmail.com>
2021-10-07 13:44:26 +0700

    buat fungsi createRoom dan genereteDetailRoom

--- controllers/gameController.js
+++ controllers/gameController.js
@@ -1,7 +1,9 @@
 const {
     User_game,
     User_game_biodata,
-    User_game_history
+    User_game_history,
+    Room,
+    Detail_room
 } = require('../models')
 
 
@@ -9,6 +11,45 @@ const getGamePage = (req, res) => {
     res.render('gameSuit')
 }
 
+const test = (req, res) => {
+    genereteFightRoom(req.params.idRoom);
+
+}
+
+const createRoom = (req, res) => {
+    Room.create({
+            nama_room: req.body.nama_room
+        })
+        .then((room) => {
+            let idUser = req.user.id;
+            let idRoom = room.id;
+            genereteDetailRoom(idRoom, idUser);
+        })
+        .then(() => {
+            res.send("berhasil create room")
+        })
+
+
+}
+
+const genereteDetailRoom = (idRoom, idUser) => {
+    let jenis_player;
+    let player;
+    for (let i = 1; i <= 2; i++) {
+        (i == 1) ? jenis_player = "player1": jenis_player = "player2";
+        (i == 1) ? player = idUser: player = null;
+        Detail_room.create({
+            id_room: idRoom,
+            id_user: player,
+            jenis_player: jenis_player
+        }).then()
+    }
+    return new Promise((resolve, reject) => {
+        resolve('berhasil');
+    })
+}
+
 module.exports = {
-    getGamePage
+    getGamePage,
+    createRoom,
 }
