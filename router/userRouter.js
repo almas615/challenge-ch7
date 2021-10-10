31afa55d82151a8ac8fb54c013a1c676ac7ab79b
almas615 <almasadlilwafi615@gmail.com>
2021-10-10 20:21:46 +0700

    tambah batasan akses super user

--- router/userRouter.js
+++ router/userRouter.js
@@ -1,13 +1,14 @@
 const router = require('express').Router();
 const user = require("../controllers/userController")
 const restrict = require('../middlewares/restrict')
+const onlySuperUser = require('../middlewares/superUser')
 
-router.get('/', user.getUsers)
+router.get('/', restrict, onlySuperUser, user.getUsers)
 
-router.post('/', user.postUsers)
+router.post('/', restrict, onlySuperUser, user.postUsers)
 
-router.get('/delete/:id', user.deleteUser)
+router.get('/delete/:id', restrict, onlySuperUser, user.deleteUser)
 
-router.post('/update/:id', user.updateUser)
+router.post('/update/:id', restrict, onlySuperUser, user.updateUser)
 
 module.exports = router;
